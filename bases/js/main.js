f232f8117c4adf333094e158773ed78dcfa2a6b3
Max2908 <maximilien.verbeke@gmail.com>
2020-10-01 19:18:54 +0200

    supprimer_tableau

--- bases/js/main.js
+++ bases/js/main.js
@@ -1,1 +1,23 @@
-console.error('test');
+// console.log('Mon message');
+// console.error('Mon message erreur');
+//document.addEventListener('click', function() {
+//    alert('MESSAGE');
+//});
+console.log("coucou");
+/*const myButton = document.getElementById('bouton-bleu');
+if(myButton != null) {
+    myButton.addEventListener('click', function(){
+       const table = document.querySelector('table');
+       table.style.display = 'none';
+    });
+}*/
+$('#bouton').on("click", function(){
+    $('table').find('tr').last().remove();
+});
+
+
+/*const btns = document.getElementsByClassName('btn');
+btns[0].addEventListener('click', function(){
+    alert('MESSAGE');
+});
+*/
